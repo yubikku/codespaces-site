d3f498c0df0246dc2e58f09124bfd47470a2982b
Jovic Gutierrez <90305484+yubikku@users.noreply.github.com>
2023-12-07 13:39:17 +0000

    Customize Template

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jovic Gutierrez",
+  title: "Programmer",
+  email: "yubikku@proton.me",
+  gitHub: "yubikku",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
+  twitter: "yubikkun",
   youTube: "Code",
 };
 
